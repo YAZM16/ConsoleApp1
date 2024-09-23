20a4bc516fc4fde19abdcec4f6083e35d93b21fb
YazTek <y.maamri16@gmail.com>
2024-09-23 13:49:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Fråga användaren om det första talet
+        Console.Write("Ange det första talet: ");
+        double tal1 = Convert.ToDouble(Console.ReadLine());
+
+        // Fråga användaren om det andra talet
+        Console.Write("Ange det andra talet: ");
+        double tal2 = Convert.ToDouble(Console.ReadLine());
+
+        // Addera talen
+        double summa = tal1 + tal2;
+
+        // Skriv ut resultatet
+        Console.WriteLine("Summan av de två talen är: " + summa);
+    }
+}
